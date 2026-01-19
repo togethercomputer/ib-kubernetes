6eaae575098c87897bf47ce0392245cb9609d097
Nik Mitra <nmitra@together.ai>
2026-01-19 00:29:31 -0800

    Add regression test

---
+++ pkg/daemon/daemon_suite_test.go
@@ -0,0 +1,13 @@
+package daemon
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestDaemon(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Daemon Suite")
+}
